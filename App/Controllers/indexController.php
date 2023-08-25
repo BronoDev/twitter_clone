185125960bcbda77768681924858bb53a83f426c
Brono <brunobrunelli1995@gmail.com>
2023-08-25 16:02:37 -0300

    MVC PHP

---
+++ App/Controllers/indexController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controllers;
+
+class IndexController
+{
+
+    public function index()
+    {
+        echo 'chegamos ao IndexController e disparamos a action index';
+    }
+
+    public function sobreNos()
+    {
+        echo 'chegamos ao IndexController e disparamos a action sobreNos';
+    }
+
+}
+
+?>
--- App/Route.php
+++ App/Route.php
@@ -2,9 +2,28 @@
 
 namespace App;
 
-class Route {
+class Route
+{
 
-	public function initRoutes() {
+	private $routes;
+
+	public function __construct()
+	{
+		$this->initRoutes();
+		$this->run($this->getUrl());
+	}
+
+	public function getRoutes()
+	{
+		return $this->routes;
+	}
+
+	public function setRoutes(array $routes)
+	{
+		$this->routes = $routes;
+	}
+	public function initRoutes()
+	{
 
 		$routes['home'] = array(
 			'route' => '/',
@@ -17,9 +36,25 @@ class Route {
 			'controller' => 'indexController',
 			'action' => 'sobreNos'
 		);
+
+		$this->setRoutes($routes);
 	}
 
-	public function getUrl() {
+	public function run($url)
+	{
+		foreach ($this->getRoutes() as $key => $route) {
+			if ($url == $route['route']) {
+				$class = "App\\Controllers\\" . ucfirst($route['controller']);
+
+				$controller = new $class;
+				$action = $route['action'];
+				$controller->$action();
+			}
+		}
+	}
+
+	public function getUrl()
+	{
 		return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 	}
 }
