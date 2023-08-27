95af523ee519f8f9cec3fba4e1f41f879ab34393
Brono <brunobrunelli1995@gmail.com>
2023-08-27 16:10:29 -0300

    MVC PHP

--- App/Controllers/indexController.php
+++ App/Controllers/indexController.php
@@ -3,19 +3,32 @@
 namespace App\Controllers;
 
 use MF\Controller\Action;
+use App\Connection;
+use App\Models\Produto;
 
 class IndexController extends Action
 {
 
     public function index()
     {
-        $this->view->dados = array('sofá', 'cadeira', 'cama');
+        //$this->view->dados = array('sofá', 'cadeira', 'cama');
+
+        //instancia de conexão
+        $conn = Connection::getBd();
+
+        //instanciar modelo
+        $produto = new Produto($conn);
+
+        $produtos = $produto->getProdutos();
+
+        @$this->view->dados = $produtos;
+
         $this->render('index', 'layout1');
     }
 
     public function sobreNos()
     {
-        $this->view->dados = array('notebook', 'smartphone');
+        //$this->view->dados = array('notebook', 'smartphone');
         $this->render('sobreNos', 'layout1');
     }
 
